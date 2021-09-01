a9bd850b8f844c37c6a59660d60df282393950f5
Amisha Sahu <amishasahu328@gmail.com>
2021-09-01 17:03:46 +0530

    Second Largest Element
    Signed-off-by: Amisha Sahu <amishasahu328@gmail.com>

---
+++ Arrays/Second_Largest.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define eff ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long 
+#define vi vector<int>
+
+signed main(){
+    eff;
+    int f = INT_MIN,s = INT_MIN;
+     
+     int n;
+     cin>>n;
+    vi v(n); 
+    for(int i = 0; i < n; i++)
+        cin>>v[i];
+
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] > f)
+        {
+          s = f;
+          f = v[i];
+        }  
+        // handle the case when all then elements in array are negative
+        else if (v[i] < f) {
+            if (s == -1 || s < v[i])
+                s = v[i];
+        }
+        // if array contails contails the duplicate elements
+        else if(s == INT_MIN)
+            s = -1;
+    }
+    cout<<s;  
+    return 0;
+}
