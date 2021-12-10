88a9d826d3fff1f5b73130de213c68796d4bb03f
Amisha Sahu <amishasahu328@gmail.com>
2021-12-10 21:31:01 +0530

    Adding Searching directory
    Signed-off-by: Amisha Sahu <amishasahu328@gmail.com>

---
+++ Searching/Allocated_Min_pages.cpp
@@ -0,0 +1,65 @@
+// Allocate Minimum number of pages
+// Minimum the maximum number of pages allocated
+// Only contiguous pages can be allocated
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define eff ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+
+bool isFeasible(vi &v, int n, int k, int mid)
+{
+    int req = 1, sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(sum+v[i] > mid)
+        {
+            req++;
+            sum = v[i];
+        }
+        else
+            sum+=v[i];
+    }
+
+    return (req <= k);
+}
+int minPages(vi &v, int n, int k)
+{
+    int sum = 0, mx = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum+=v[i];
+        mx=max(mx, v[i]);
+    }
+
+    int low = mx, high = sum, res = 0;
+    while(low <= high)
+    {
+        int mid = (low+high)/2;
+        if(isFeasible(v, n, k, mid))
+        {
+            res = mid;
+            high = mid-1;
+        }
+        else
+            low = mid+1;
+    }
+    return res;
+}
+signed main(){
+    eff;
+    int n,k;
+    cin>>n>>k;
+    vi v(n);
+    for(int i = 0; i < n; i++)
+        cin>>v[i];
+
+    
+    cout<<minPages(v, n, k);
+    
+
+    return 0;
+}
+// Time complexity: O(nlog(sum))
+// Auxiliary space: O(1)
