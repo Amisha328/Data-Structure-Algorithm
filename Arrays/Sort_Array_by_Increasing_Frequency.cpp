d656196ffe9c27f5a673361a22071a90734c3513
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2022-01-15 15:53:24 +0530

    Sorting the array wrt increasing frequency

---
+++ Arrays/Sort_Array_by_Increasing_Frequency.cpp
@@ -0,0 +1,19 @@
+// Given an array of integers nums, sort the array in increasing order based on the frequency of the values.
+// If multiple values have the same frequency, sort them in decreasing order.
+
+#include<bits/stdc++.h>
+using namespace std;
+#define eff ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+
+signed main(){
+    eff;
+    int n;
+    cin>>n;
+    vi v(n);
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+
+    
+    return 0;
+}
