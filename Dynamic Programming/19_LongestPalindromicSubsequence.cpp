d814da8ad2b165f9b3ae03bf582b0b096b427082
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2022-01-31 20:18:32 +0530

    Longest Palindromic Subsequence

---
+++ Dynamic Programming/19_LongestPalindromicSubsequence.cpp
@@ -0,0 +1,48 @@
+// Longest Palindromic Subsequence
+// Problem Statement: https://www.geeksforgeeks.org/longest-palindromic-subsequence-dp-12/
+// Problem linK: https://leetcode.com/problems/longest-palindromic-subsequence/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+  public:
+    int longestPalinSubseq(string A) 
+    {
+        string B = A;
+        reverse(B.begin(), B.end());
+        
+        int n = A.size();
+        
+        int dp[n+1][n+1];
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= n; j++)
+            {
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(A[i-1] == B[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[n][n];
+    }
+};
+
+int main()
+{
+    string s; cin >> s;
+    Solution ob;
+    cout << ob.longestPalinSubseq(s) << endl;
+}
