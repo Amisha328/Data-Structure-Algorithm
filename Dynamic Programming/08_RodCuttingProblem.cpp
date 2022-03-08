e532dd5766c6bb34357c35f1b54439e79c6eb52c
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2022-03-08 13:43:43 +0530

    update rod cutting problem
    Added memoization, tabulation and space optimization

--- Dynamic Programming/08_RodCuttingProblem.cpp
+++ Dynamic Programming/08_RodCuttingProblem.cpp
@@ -1,11 +1,108 @@
 // Problem statement: https://practice.geeksforgeeks.org/problems/rod-cutting0840/1/#
-
+// https://www.codingninjas.com/codestudio/problem-details/rod-cutting-problem_800284
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution{
   public:
+  /*
+    // Memoization:
+    int solve(vector<int> &price, int idx, int N, vector<vector<int>> &dp)
+    {
+        if(idx == 0)
+            return N*price[0];
+        if(dp[idx][N] != -1) return dp[idx][N];
+        
+        int notTake = 0 + solve(price, idx-1, N, dp);
+        int take = INT_MIN;
+        int rodLen = idx+1;
+        if(rodLen <= N) 
+            take = price[idx] + solve(price, idx, N-rodLen, dp);
+        
+        return dp[idx][N] = max(take, notTake);
+        
+    }
+    int cutRod(vector<int> &price, int n)
+    {
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return solve(price, n-1, n, dp);   
+    }
+*/
+    /*
+    // Tabulation:
+    int cutRod(vector<int> &price, int n)
+    {
+        vector<vector<int>> dp(n, vector<int>(n+1, 0));
+        for(int N = 0; N <= n; N++)
+            dp[0][N] = N * price[0];
+        for(int idx = 1; idx < n; idx++)
+        {
+            for(int N = 0; N <= n; N++)
+            {
+                int notTake = 0 + dp[idx-1][N];
+                int take = INT_MIN;
+                int rodLen = idx+1;
+                if(rodLen <= N) 
+                    take = price[idx] + dp[idx][N-rodLen];
+
+                dp[idx][N] = max(take, notTake);
+            }
+        }
+        return dp[n-1][n];
+    }*/
+
+    /*
+    // Space Optimization:
+    int cutRod(vector<int> &price, int n)
+    {
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
+        
+        for(int N = 0; N <= n; N++)
+            prev[N] = N * price[0];
+        for(int idx = 1; idx < n; idx++)
+        {
+            for(int N = 0; N <= n; N++)
+            {
+                int notTake = 0 + prev[N];
+                int take = INT_MIN;
+                int rodLen = idx+1;
+                if(rodLen <= N) 
+                    take = price[idx] + curr[N-rodLen];
+
+                curr[N] = max(take, notTake);
+            }
+            prev = curr;
+        }
+        return prev[n];
+    }
+    */
+    
+    //  More optimized Solution:
+    int cutRod(vector<int> &price, int n)
+    {
+        vector<int> prev(n+1, 0);
+        for(int N = 0; N <= n; N++)
+            prev[N] = N * price[0];
+        for(int idx = 1; idx < n; idx++)
+        {
+            for(int N = 0; N <= n; N++)
+            {
+                int notTake = 0 + prev[N];
+                int take = INT_MIN;
+                int rodLen = idx+1;
+                if(rodLen <= N) 
+                    take = price[idx] + prev[N-rodLen];
+
+                prev[N] = max(take, notTake);
+            }
+        }
+        return prev[n];
+    }
+
+    /*
+    // Aditya Verma's Solution
     int cutRod(int price[], int n) {
         //code here
         int len[n+1];
@@ -35,6 +132,7 @@ class Solution{
         
         return dp[n][n];
     }
+    */
 };
 
 
@@ -42,12 +140,12 @@ int main()
 {
         int n;
         cin >> n;
-        int a[n];
+       vector<int> price(n);
         for (int i = 0; i < n; i++) 
-            cin >> a[i];
+            cin >> price[i];
             
         Solution ob;
 
-        cout << ob.cutRod(a, n) << endl;
+        cout << ob.cutRod(price, n) << endl;
     return 0;
 }  
