8bbb26bb669fb3ff27d32c47dcd99187a52bc0b9
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2022-03-10 00:03:55 +0530

    Update 12_LCS_DP.cpp

--- Dynamic Programming/12_LCS_DP.cpp
+++ Dynamic Programming/12_LCS_DP.cpp
@@ -1,6 +1,7 @@
-// Longest Common Subsequence - Top - down approach
+// Longest Common Subsequence - Bottom-up approach
 
 // Problem Statement: https://leetcode.com/problems/longest-common-subsequence/
+// https://www.codingninjas.com/codestudio/problem-details/longest-common-subsequence_1063255
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -16,13 +17,9 @@ class Solution
         int dp[n+1][m+1];
         
         for(int i = 0; i <= n; i++)
-        {
-            for(int j = 0; j <= m; j++)
-            {
-                if(i == 0 || j == 0)
-                    dp[i][j] = 0;
-            }
-        }
+            dp[i][0] = 0;
+        for(int j = 0; j <= m; j++)
+            dp[0][j] = 0;
         
         for(int i = 1; i <= n; i++)
         {
