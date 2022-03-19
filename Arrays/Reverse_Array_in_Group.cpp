c8bfa6897e53c9155e83717ca2da10f89fce3739
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2022-03-19 09:48:22 +0530

    Reverse an array in group of size k

---
+++ Arrays/Reverse_Array_in_Group.cpp
@@ -0,0 +1,45 @@
+//Problem statement: https://www.geeksforgeeks.org/rearrange-array-maximum-minimum-form/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    //Function to reverse every sub-array group of size k.
+    // TC->O(n) | SC->O(1)
+    void reverseInGroups(vector<long long>& arr, int n, int k){  
+        for(int i = 0; i < n; i+=k)
+        {
+            if(i+k >= n)
+                reverse(arr.begin()+i, arr.end());
+            else
+                reverse(arr.begin()+i, arr.begin()+i+k);
+        }
+    }
+};
+
+int main() {
+    int t; 
+    cin >> t; 
+    while(t--){ 
+        int n;
+        cin >> n; 
+        vector<long long> arr; 
+        int k;
+        cin >> k; 
+
+        for(long long i = 0; i<n; i++)
+        {
+            long long x;
+            cin >> x; 
+            arr.push_back(x); 
+        }
+        Solution ob;
+        ob.reverseInGroups(arr, n, k);
+        
+        for(long long i = 0; i<n; i++){
+            cout << arr[i] << " "; 
+        }
+        cout << endl;
+    }
+}
