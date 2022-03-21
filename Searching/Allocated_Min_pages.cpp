002a39d52ca24ca2e2007eb2b99d84ff28d5607e
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2022-03-22 00:17:25 +0530

    Update Allocated_Min_pages.cpp

--- Searching/Allocated_Min_pages.cpp
+++ Searching/Allocated_Min_pages.cpp
@@ -1,7 +1,8 @@
 // Allocate Minimum number of pages
 // Minimum the maximum number of pages allocated
 // Only contiguous pages can be allocated
-
+// Problem link: https://practice.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1#
+// https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -13,7 +14,8 @@ bool isFeasible(vi &v, int n, int k, int mid)
     int req = 1, sum = 0;
     for(int i = 0; i < n; i++)
     {
-        if(sum+v[i] > mid)
+        if(v[i] > mid) return false;
+        else if(sum+v[i] > mid)
         {
             req++;
             sum = v[i];
@@ -29,8 +31,8 @@ int minPages(vi &v, int n, int k)
     int sum = 0, mx = 0;
     for(int i = 0; i < n; i++)
     {
-        sum+=v[i];
-        mx=max(mx, v[i]);
+        sum += v[i];
+        mx = min(mx, v[i]);
     }
 
     int low = mx, high = sum, res = 0;
