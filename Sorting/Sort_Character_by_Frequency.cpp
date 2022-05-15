cad074d509602dd04f0ac79db5d09ff29ed93107
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2022-05-15 14:27:21 +0530

    Update Sort_Character_by_Frequency.cpp

--- Sorting/Sort_Character_by_Frequency.cpp
+++ Sorting/Sort_Character_by_Frequency.cpp
@@ -12,9 +12,33 @@ public:
         for(int i = 0; i < s.size(); i++)
             mp[s[i]]++;
         
+        /*
+        // Using STL sorting
+        // O(nlogn)
          sort(s.begin(), s.end(), [&](char a, char b) -> bool {
            return (mp[a] != mp[b]? mp[a] > mp[b] : a < b);
         });
+        */
+        
+        /*
+        The idea is to use max heap with nodes containing a pair {frequency, character}. 
+        Since the first element of the pair is frequency, we don't need to pass a custom comparator.           
+        Max heap will be constructed on the basis of frequency of characters, i.e. the root will contain the character with maximum frequency.*/
+
+
+        // Using Max-heap -> freq, char
+        // O(log(n))
+        priority_queue<pair<int, char>> pq;
+        for(auto it: mp){
+            pq.push({it.second, it.first});
+        }
+        
+        // O(n)
+        s = "";
+        while(!pq.empty()){
+            s += string(pq.top().first, pq.top().second); // frequency times the character
+            pq.pop();
+        }
         
         return s;
     }
