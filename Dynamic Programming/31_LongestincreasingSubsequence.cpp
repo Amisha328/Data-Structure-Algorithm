4b6308f470f26ed436f95d80e2727c8870e61985
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2023-04-10 18:48:22 +0530

    Longest Increasing Subsequence - C++

--- Dynamic Programming/31_LongestincreasingSubsequence.cpp
+++ Dynamic Programming/31_LongestincreasingSubsequence.cpp
@@ -1,89 +1,139 @@
 // Longest Increasing Subsequence
-// Problem Statement: https://leetcode.com/problems/longest-increasing-subsequence/
+// Problem Statement: 
+// https://leetcode.com/problems/longest-increasing-subsequence/
+// https://practice.geeksforgeeks.org/problems/longest-increasing-subsequence-1587115620/1?
+// https://www.codingninjas.com/codestudio/problems/longest-increasing-subsequence_630459?leftPanelTab=0
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Simple recursive code
+// TC -> O(2 ^ N) | SC -> O(N)
 class Solution {
-public:
-/*
-// Binary Search Approach - 
-// time complexity: O(nlogn)
-// space complexity: O(n)
-
-    // Binary seach to find the index of ceiling value in tail array
-    int ceilIndex(int tail[], int l, int h, int val)
-    {
-        while(l < h)
-        {
-            int m = (l+h)/2;
-            if(tail[m] >= val)
-                h = m;
-            else
-                l = m+1;
+    private:
+        int f(int idx, int prev_idx, vector<int>& nums, int n){
+            if(idx == n) return 0;
+            int len = 0 + f(idx+1, prev_idx, nums, n);
+            if(prev_idx == -1 || nums[idx] > nums[prev_idx])
+                len = max(len, 1 + f(idx+1, idx, nums, n));
+            return len;
         }
-        return h;
-    }
+    public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int tail[n];
-        int len = 1;
-        tail[0] = nums[0];
-        
-        for(int i = 1; i < n; i++)
-        {
-            // insert in tail array if higher number is found
-            if(tail[len-1] < nums[i])
-                tail[len++] = nums[i];
-            else
-            {
-                // replace the current number with the ceiling value in sorted array tail
-                // the ceiling of x is the smallest element in array greater than or equal to x
-                int c = ceilIndex(tail, 0, len-1, nums[i]);
-                tail[c] = nums[i];
-            }
-        }
-        
-        return len;
+        return f(0, -1, nums, n);
     }
-    */
-
-          // Dynamic Programming Approach - 
-          // Time complexity: O(n^2)
-          // Space complexity: O(n)
-          int lengthOfLIS(vector<int>& nums) {
-                    int n = nums.size();
-                    int lis[n];
-                    lis[0] = 1;
-                    for(int i = 1; i < n; i++)
-                    {
-                              lis[i] = 1;
-                              for(int j = 0; j < i; j++)
-                              {
-                                        if(nums[j] < nums[i])
-                                                  lis[i] = max(lis[i], lis[j]+1);
-                              }
-                    }
-                    return *max_element(lis, lis+n);
-          }
-
-
 };
 
-int main()
+// Memoization:
+// TC -> O(N X N) | SC -> O(N X N) + O(N)
+
+class Solution {
+private:
+    int f(int idx, int prev_idx, vector<int>& nums, vector<vector<int>> &dp){
+        if(idx == nums.size()) return 0;
+        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1]; // prev_idx+1 due to co-ordinate change
+        int len = 0;
+        len = 0 + f(idx+1, prev_idx, nums, dp);
+        if(prev_idx == -1 || nums[idx] > nums[prev_idx])
+            len = max(len, 1 + f(idx+1, idx, nums, dp));
+        return dp[idx][prev_idx+1] = len; // prev_idx+1 due to co-ordinate change
+    }
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        // we are using dp of n X (n+1) as 
+        // idx ranges from 0 to n-1 and prev_idx ranges from -1 to (n-1) so we are doing co-cordinate shifting from 0 based indexing to 1 based indexing for prev_idx
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return f(0, -1, nums, dp);
+    }
+};
+
+// Tabulation:
+// TC -> O(N X N) | SC -> O(N X N)
+
+
+class Solution {
+public:
+int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int idx = n-1; idx >= 0; idx--){
+            // weare not taking prev idx from n-1 to -1 beacause according to our logic we just want the prev index element
+            for(int prev = idx-1; prev >= -1; prev--){
+                int len = 0;
+                // making co-ordinate change at all the place for prev index
+                len = 0 + dp[idx+1][prev+1];
+                if(prev == -1 || nums[idx] > nums[prev])
+                    len = max(len, 1 + dp[idx+1][idx+1]);
+                dp[idx][prev+1] = len; // prev_idx+1 due to co-ordinate change
+            }
+        }
+        return dp[0][-1+1];
+    }
+};
+
+// Space Optimization:
+// TC -> O(N X N) | SC -> O(N X 2)
+
+class Solution {
+public:
+int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ahead(n+1, 0), curr(n+1, 0);
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int prev = idx-1; prev >= -1; prev--){
+                int len = 0;
+                len = 0 + ahead[prev+1];
+                if(prev == -1 || nums[idx] > nums[prev])
+                    len = max(len, 1 + ahead[idx+1]);
+               curr[prev+1] = len; 
+            }
+            ahead = curr;
+        }
+        return ahead[-1+1];
+    }
+};
+
+// Further Optimization
+// TC -> O(N X N) | SC -> O(N)
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int maxi = 0;
+        for(int i = 0; i < n; i++){
+            for(int prev = 0; prev < i; prev++){
+                if(nums[prev] < nums[i])
+                    dp[i] = max(dp[i], 1 + dp[prev]);
+            }
+            maxi = max(maxi, dp[i]);
+        }
+        return maxi;
+    }
+};
+
+
+// Binary Serach Code:
+// TC-> O(NlogN) | SC -> O(N)
+int longestIncreasingSubsequence(int arr[], int n)
 {
-          Solution s;
-          int n;
-          cin>>n;
-          vector<int> nums;
-          for(int i = 0; i < n; i++)
-          {
-              int x;
-              cin>>x;
-              nums.push_back(x);
-          }
-          cout << s.lengthOfLIS(nums) << endl;
-          return 0;
+    vector<int> temp;
+    temp.push_back(arr[0]);
+    int len = 1;
+    for(int i = 1; i < n; i++){
+        if(arr[i] > temp.back()){
+            temp.push_back(arr[i]);
+            len++;
+        }
+        else{
+            int idx = lower_bound(temp.begin(), temp.end(), arr[i])-temp.begin();
+            temp[idx] = arr[i];
+        }
+    }
+    return len;
 }
-
-
