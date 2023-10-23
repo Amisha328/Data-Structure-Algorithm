a110be98c7328f2375d2ec9f2c97a3304dd1ec57
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2023-10-23 21:17:55 +0530

    Add files via upload

---
+++ Searching/09_Maximum_Candies.cpp
@@ -0,0 +1,53 @@
+// Maximum Candies
+/*
+You are at a candy shop, which has an unlimited type of candies and the number of candies of the i−th type is i. Each candy costs 1
+1 rupee and you have n rupees. If you purchase a particular type of candy, you need to purchase all the candies of that type and the types less than that. Find the maximum number of types of candies you can purchase with n
+rupees. For e.g. if you purchase 2 types of candies, you need to purchase 1 unit of the first type and 2 units of the second type.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define eff ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long 
+
+// the last max value for which the below condition satisfies
+// so, we will take the last occurnace (L)
+ll predicate(ll x, ll n){
+   if((x*(x+1))/2 <= n) return 0;
+   else return 1;
+}
+int main(){
+    eff;
+    ll n;
+    cin >> n;
+    // Step 1: Monotonically increasing
+    // Step 2: Cutoff point - n
+    // Step 3: Code:
+    ll L = 0, R = 2 * 1e9;
+    /*
+    Reason for taking R = 2 * 1e9
+
+    Rule:
+    According to rule of invarient - SBA
+        l should remain in block 0
+        r should remian in block 1
+        then,
+        predicate(l) should always return 0
+        predicate(r) should always return 1
+        
+    N is at max 10^18
+    so, for r*(r+1)/2 = r*r 
+    that is, if r = 10^9  then r*r = 10^18
+
+    to get the min value of r for which the predicate function will return 1
+    r must be slightly greater than 10^9 -> 2 * 10^9
+    */
+    while(L + 1 < R){
+      ll M = L + (R - L)/2;
+      if(predicate(M, n) == 0) L = M;
+      else R = M;
+    }
+    cout<<L<<endl;
+  return 0;
+
+}
